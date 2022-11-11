9783e9ababcfd551f0436f2a146aedfc5b7dd728
Pablo Valente <pablo.nvalente@gmail.com>
2022-11-11 17:53:48 -0300

    add actions menu buttons

--- src/components/ActionButtons/DeleteButton.tsx
+++ src/components/ActionButtons/DeleteButton.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import { AiFillDelete, AiOutlineDelete } from 'react-icons/ai';
+import { Todo } from '../../todoModel';
 
-const EditButton = () => {
-  const [isHover, setIsHover] = useState(false);
+type props = {
+  todo: Todo;
+  handleDelete: (id: number) => void;
+};
+
+const EditButton = ({ todo, handleDelete }: props) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
   return (
-    <span onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
-      {isHover ? (
-        <AiFillDelete className='w-5 h-5 cursor-pointer' />
-      ) : (
-        <AiOutlineDelete className='w-5 h-5 cursor-pointer' />
-      )}
+    <span
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+      onClick={() => handleDelete(todo.id)}
+      className='flex items-center w-full gap-2 p-2 text-left rounded-md cursor-pointer ui-active:bg-slate-300/30'>
+      <span>
+        {isHover ? (
+          <AiFillDelete className='w-5 h-5 cursor-pointer' />
+        ) : (
+          <AiOutlineDelete className='w-5 h-5 cursor-pointer' />
+        )}
+      </span>
+      Delete
     </span>
   );
 };
