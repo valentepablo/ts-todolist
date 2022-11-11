8097886c090c03184670d7eb0dd9793a526ee231
Pablo Valente <pablo.nvalente@gmail.com>
2022-11-11 17:54:37 -0300

    add actions menu buttons (#1)

--- src/components/ActionButtons/CompleteButton.tsx
+++ src/components/ActionButtons/CompleteButton.tsx
@@ -1,17 +1,33 @@
 import { useState } from 'react';
 import { AiOutlineCheckSquare, AiFillCheckSquare } from 'react-icons/ai';
+import { Todo } from '../../todoModel';
 
-const EditButton = () => {
-  const [isHover, setIsHover] = useState(false);
+type props = {
+  todo: Todo;
+  handleCompleted: (id: number) => void;
+};
+
+const CompleteButton = ({ todo, handleCompleted }: props) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
   return (
-    <span onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
-      {isHover ? (
-        <AiFillCheckSquare className='w-5 h-5 cursor-pointer' />
-      ) : (
-        <AiOutlineCheckSquare className='w-5 h-5 cursor-pointer' />
-      )}
+    <span
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+      onClick={() => handleCompleted(todo.id)}
+      className={`${
+        todo.completed && 'pointer-events-none text-slate-400'
+      } flex items-center gap-2 w-full cursor-pointer p-2 text-left rounded-md ui-active:bg-slate-300/30`}>
+      <span>
+        {isHover ? (
+          <AiFillCheckSquare className='w-5 h-5 cursor-pointer' />
+        ) : (
+          <AiOutlineCheckSquare className='w-5 h-5 cursor-pointer' />
+        )}
+      </span>
+      Complete
     </span>
   );
 };
 
-export default EditButton;
+export default CompleteButton;
