98271ef51f7097db4ffab3201c42df6edf5dc090
Pablo Valente <pablo.nvalente@gmail.com>
2022-11-17 17:53:57 -0300

    add animations to dropdown menus

--- src/components/ActionsButton.tsx
+++ src/components/ActionsButton.tsx
@@ -4,6 +4,7 @@ import EditButton from './ActionButtons/EditButton';
 import DeleteButton from './ActionButtons/DeleteButton';
 import CompleteButton from './ActionButtons/CompleteButton';
 import { HiChevronDown, HiDotsVertical } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 type props = {
   todo: Todo;
@@ -14,33 +15,48 @@ type props = {
 
 const ActionsButton = ({ todo, handleEdit, handleDelete, handleCompleted }: props) => {
   return (
-    <Menu as='div' className='relative'>
-      <Menu.Button className='items-center hidden p-2 text-sm text-blue-600 rounded-lg md:flex bg-slate-300/30'>
-        Options
-        <HiChevronDown className='w-4 h-4' />
-      </Menu.Button>
-      <Menu.Button className='items-center block p-2 text-sm text-blue-600 rounded-lg md:hidden bg-slate-300/30'>
-        <HiDotsVertical className='w-4 h-4' />
-      </Menu.Button>
-      <Menu.Items className='absolute right-0 z-10 p-1 space-y-2 text-sm rounded-lg shadow-md top-10 w-36 bg-slate-100'>
-        <Menu.Item>
-          <div className={`${todo.completed && 'pointer-events-none'} rounded-md`}>
-            <EditButton todo={todo} handleEdit={handleEdit} />
-          </div>
-        </Menu.Item>
-        <Menu.Item>
-          <div className={`${todo.completed && 'pointer-events-none'} rounded-md`}>
-            <CompleteButton todo={todo} handleCompleted={handleCompleted} />
-          </div>
-        </Menu.Item>
-        <div className='border-t'></div>
-        <Menu.Item>
-          <div className='rounded-md'>
-            <DeleteButton todo={todo} handleDelete={handleDelete} />
-          </div>
-        </Menu.Item>
-      </Menu.Items>
-    </Menu>
+    <div className='relative'>
+      <Menu>
+        {({ open }) => (
+          <>
+            <Menu.Button className='items-center hidden p-2 text-sm text-blue-600 rounded-lg md:flex bg-slate-300/30'>
+              Options
+              <HiChevronDown className='w-4 h-4' />
+            </Menu.Button>
+            <Menu.Button className='items-center block p-2 text-sm text-blue-600 rounded-lg md:hidden bg-slate-300/30'>
+              <HiDotsVertical className='w-4 h-4' />
+            </Menu.Button>
+
+            {open && (
+              <Menu.Items
+                as={motion.div}
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: 'auto' }}
+                transition={{ duration: 0.15 }}
+                static
+                className='absolute right-0 z-10 p-1 space-y-2 text-sm rounded-lg shadow-md top-10 w-36 bg-slate-100'>
+                <Menu.Item>
+                  <div className={`${todo.completed && 'pointer-events-none'} rounded-md`}>
+                    <EditButton todo={todo} handleEdit={handleEdit} />
+                  </div>
+                </Menu.Item>
+                <Menu.Item>
+                  <div className={`${todo.completed && 'pointer-events-none'} rounded-md`}>
+                    <CompleteButton todo={todo} handleCompleted={handleCompleted} />
+                  </div>
+                </Menu.Item>
+                <div className='border-t'></div>
+                <Menu.Item>
+                  <div className='rounded-md'>
+                    <DeleteButton todo={todo} handleDelete={handleDelete} />
+                  </div>
+                </Menu.Item>
+              </Menu.Items>
+            )}
+          </>
+        )}
+      </Menu>
+    </div>
   );
 };
 
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,6 +1,7 @@
 import { Listbox } from '@headlessui/react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { priorities } from './utils/priorities';
+import { motion } from 'framer-motion';
 
 type props = {
   todo: string;
@@ -22,20 +23,38 @@ const InputField = ({ todo, setTodo, handleAdd, priority, setPriority }: props) 
       />
 
       <Listbox value={priority} onChange={setPriority} as='div' className='relative'>
-        <Listbox.Button className='w-20 py-2 capitalize border-l bg-slate-800 text-slate-400 hover:text-slate-300 border-slate-700 hover:bg-slate-600'>
-          {priority}
-        </Listbox.Button>
-        <Listbox.Options className='absolute right-0 z-10 p-1 mt-1 space-y-2 text-sm rounded-lg shadow-md top-10 w-36 bg-slate-100'>
-          {priorities.map((priority) => (
-            <Listbox.Option
-              value={priority.name}
-              key={priority.name}
-              className={`flex items-center gap-2 p-2 font-semibold capitalize rounded-md cursor-pointer ${priority.textColor} hover:bg-slate-300/30`}>
-              <span className={`${priority.bgColor} inline-block w-3 rounded-full aspect-square`} />
-              {priority.name}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
+        {({ open }) => (
+          <>
+            <Listbox.Button className='w-20 py-2 capitalize border-l bg-slate-800 text-slate-400 hover:text-slate-300 border-slate-700 hover:bg-slate-600'>
+              {priority}
+            </Listbox.Button>
+            {open && (
+              <Listbox.Options
+                as={motion.div}
+                static
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: 'auto' }}
+                transition={{ duration: 0.15 }}
+                className='absolute right-0 z-10 p-1 mt-1 space-y-2 text-sm rounded-lg shadow-md top-10 w-36 bg-slate-100'>
+                {priorities.map((priority) => (
+                  <Listbox.Option
+                    as={motion.div}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.3 }}
+                    value={priority.name}
+                    key={priority.name}
+                    className={`flex items-center gap-2 p-2 font-semibold capitalize rounded-md cursor-pointer ${priority.textColor} hover:bg-slate-300/30`}>
+                    <span
+                      className={`${priority.bgColor} inline-block w-3 rounded-full aspect-square`}
+                    />
+                    {priority.name}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            )}
+          </>
+        )}
       </Listbox>
 
       <button
